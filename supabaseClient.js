74593aaf3b71c08d4b0436a45ce197643c62516a
Nj <nj@Njs-MacBook-Neo.local>
2026-07-26 10:31:46 +0300

    Your message

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 
-const SUPABASE_URL = 'https://aidsklvpzodubkxiwcjs.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_1z4thc4qJUhsv3z9lVOrPw_iZu0kXBL';
+const SUPABASE_URL = 'YOUR_SUPABASE_URL';
+const SUPABASE_PUBLISHABLE_KEY = 'YOUR_SUPABASE_ANON_KEY';
 
 // Initialize the Supabase Client and attach it to the window object so all pages can use it
 window.supabaseApp = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
